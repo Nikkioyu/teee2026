7f3cf6ae6986b12ea21f872affd79a26c45edc7c
oyundari <Ganaabridge@gmail.com>
2026-05-31 13:04:28 +0800

    Geriin daalgavar 5/24

---
+++ 3-r bodlogo 5/5/Geriindaalgavar.py
@@ -0,0 +1,11 @@
+import random
+too = random.randint (1, 20)
+while True:
+guess = int(input("1-20 toog taa: "
+if guess › too :
+print("arai ih bn ")
+elif guess < too:
+   print("arai baga bn ")
+else :
+   print("zuv taalaa!")
+break
